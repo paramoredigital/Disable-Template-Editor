5fe81671666945017a277abb0c1f4f6b01206de9
David Ordal <david@ordal.com>
2012-08-04 18:38:22 -0700

    unhid submit button on template view page (so preferences can be changed); used nth-child() for the 'new template' and 'delete group' buttons, and added help text.

--- ext.disable_template_editor.php
+++ ext.disable_template_editor.php
@@ -87,15 +87,14 @@ class Disable_template_editor_ext
 	public function inject_cp_js()
 	{
 		$str = '$(function() {' .
-			'$("#templateEditor input[type=\"submit\"]").hide();' .
 			'$("#templateEditor input[name=\"save_template_file\"]").parent().hide();' .
 			'$("#templateEditor #template_details > p").html("Read Only (Source Controlled) &ndash;" + $("#templateEditor #template_details > p").html());' .
 			'$("#templateEditor textarea[name=\"template_data\"]").attr("readonly", "readonly");' .
 			'$("#templateGroups .newTemplate").hide();' .
-			'$("label[for=\"name_of_template_group\"]").parent().parent().hide();' .
-			'$(".templateGrouping div.newTemplate:eq(1)").hide();' .
-			'$(".templateGrouping div.newTemplate:eq(0)").hide();' .
+			'$(".templateGrouping div.newTemplate:nth-child(2)").hide();' .
+			'$(".templateGrouping div.newTemplate:nth-child(1)").hide();' .
 			'$(".templateEditorTop > h2").html("Template Management (Under Source Control)");' .
+			'$(".templateEditorTop").append("<div style=\'clear: both\'>All templates &amp; template groups must be created and edited via the templates folder on the filesystem.</div>");' .
 			'$(".templateTable input[name=\"template_name\"]").attr("disabled", "disabled");' .
 			'$(".templateTable .template_manager_template_name").html($(".templateTable .template_manager_template_name").html() + " (Read Only)");' .
 		'});';
